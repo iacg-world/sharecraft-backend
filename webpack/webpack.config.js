7cb59b9eb9f9241fd8795fdcaedd8fd034f6e579
supercll <1033581609@qq.com>
2022-06-25 20:07:32 +0800

    config: 配置开发与生产环境

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -7,43 +7,48 @@ const FileManagerPlugin = require('filemanager-webpack-plugin')
 const buildFileDest = path.resolve(__dirname, '../app/public')
 const templateFileDest = path.resolve(__dirname, '../app/view')
 
-module.exports = {
-  mode: 'production',
-  context: path.resolve(__dirname, '../webpack'),
-  entry: './index.js',
-  output: {
-    path: buildFileDest,
-    filename: 'bundle.[hash].js',
-    publicPath: '/public/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'page.nj',
-      template: path.resolve(__dirname, './template.html'),
-    }),
-    new FileManagerPlugin({
-      events: {
-        onEnd: {
-          copy: [
-            {
-              source: path.join(buildFileDest, 'page.nj'),
-              destination: path.join(templateFileDest, 'page.nj'),
-            },
-          ],
+module.exports = env => {
+  console.log('env', env.production)
+  return {
+    mode: 'production',
+    context: path.resolve(__dirname, '../webpack'),
+    entry: './index.js',
+    output: {
+      path: buildFileDest,
+      filename: 'bundle.[hash].js',
+      publicPath: env.production
+        ? 'http://sharecraft-backend.oss-cn-shanghai.aliyuncs.com/h5-assets/'
+        : '/public/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
-      },
-    }),
-  ],
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css',
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'page.nj',
+        template: path.resolve(__dirname, './template.html'),
+      }),
+      new FileManagerPlugin({
+        events: {
+          onEnd: {
+            copy: [
+              {
+                source: path.join(buildFileDest, 'page.nj'),
+                destination: path.join(templateFileDest, 'page.nj'),
+              },
+            ],
+          },
+        },
+      }),
+    ],
+  }
 }
