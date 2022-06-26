ac80e65a99df7b96c8b04e04849735f37fe51b7c
supercll <1033581609@qq.com>
2022-06-26 17:53:56 +0800

    feat: 引入casl，用户角色模型

--- app/model/user.ts
+++ app/model/user.ts
@@ -12,6 +12,7 @@ export interface UserProps {
   type: 'email' | 'cellphone' | 'oauth'
   provider?: 'gitee'
   oauthID?: string
+  role?: 'admin' | 'normal'
 }
 
 function initUserModel(app: Application) {
@@ -29,6 +30,7 @@ function initUserModel(app: Application) {
       type: { type: String, default: 'email' },
       provider: { type: String },
       oauthID: { type: String },
+      role: { type: String, default: 'normal' },
     },
     {
       timestamps: true,
