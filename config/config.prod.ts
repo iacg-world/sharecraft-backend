44384265167308391069aa36a654c966641e9b8d
supercll <1033581609@qq.com>
2022-07-10 17:49:49 +0800

    feat: docker-compose的使用

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -3,5 +3,8 @@ import { EggAppConfig, PowerPartial } from 'egg'
 export default () => {
   const config: PowerPartial<EggAppConfig> = {}
   config.baseUrl = 'prod.url'
+  config.mongoose = {
+    url: 'mongodb://craft-mongo:27017/sharecraft',
+  }
   return config
 }
