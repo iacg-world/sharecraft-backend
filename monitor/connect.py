4747b7d481f10e1fb828fa54566c83d1c73e59c1
supercll <1033581609@qq.com>
2023-05-09 11:11:53 +0800

    chore: get envPath

--- monitor/connect.py
+++ monitor/connect.py
@@ -2,8 +2,14 @@ from odps import ODPS
 import sys
 import urllib
 from dotenv import dotenv_values
+import os
 
-env_variables = dotenv_values('../.env')
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+parent_dir = os.path.dirname(script_dir)
+env_path = os.path.join(parent_dir, '.env')
+
+env_variables = dotenv_values(env_path)
 AccessKey = env_variables['MONITOR_ACCESS_KEY']
 secret = env_variables['MONITOR_SECRET_KEY']
 
