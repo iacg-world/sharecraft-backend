060da9f2b6acb2868ea5426ee2f950850f28b739
supercll <1033581609@qq.com>
2023-05-26 10:20:24 +0800

    feat: 取消服务端cors

--- typings/config/plugin.d.ts
+++ typings/config/plugin.d.ts
@@ -20,7 +20,6 @@ import 'egg-validate';
 import 'egg-bcrypt';
 import 'egg-jwt';
 import 'egg-redis';
-import 'egg-cors';
 import 'egg-oss';
 import { EggPluginItem } from 'egg';
 declare module 'egg' {
@@ -43,7 +42,6 @@ declare module 'egg' {
     bcrypt?: EggPluginItem;
     jwt?: EggPluginItem;
     redis?: EggPluginItem;
-    cors?: EggPluginItem;
     oss?: EggPluginItem;
   }
 }
